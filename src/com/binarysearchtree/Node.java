b4ef0936b80de874b5e7cfc6f0a748b026a3c78b
Amit <amitlpalandurkar1998@gmail.com>
2023-07-06 13:54:57 +0530

    Create Binary Search Tree.

--- src/com/binarysearchtree/Node.java
+++ src/com/binarysearchtree/Node.java
@@ -1,5 +1,10 @@
 package com.binarysearchtree;
 
 public class Node {
-
+    int data;
+    Node right , left;
+    public Node(int data){
+        this.data=data;
+        this.right=this.left=null;
+    }
 }
